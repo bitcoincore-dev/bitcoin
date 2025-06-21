2eaefff591bf606575e8b137d6aab5b2a58fff43
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-06-21 13:58:56 -0400

    cmake-build-macos.sh:for testing

---
+++ cmake-build-macos.sh
@@ -0,0 +1,28 @@
+compiler=${1:-clang++}
+# Go up one directory from 'build' to 'bitcoin'
+pushd . || exit
+
+# Remove the old build directory to ensure a clean slate
+# rm -rf build || true
+
+# Create a new build directory
+mkdir -p build
+
+# Change into the new build directory
+pushd ./build || exit
+
+if command -v brew &> /dev/null
+then
+    brew install llvm
+    export LDFLAGS="-L/usr/local/opt/llvm/lib"
+    export CPPFLAGS="-I/usr/local/opt/llvm/include"
+    CC=$(brew --prefix llvm)/bin/clang CXX=$(brew --prefix llvm)/bin/clang++
+fi
+
+# Run CMake, explicitly setting the C++ standard to 20
+# We'll also explicitly set the C++ compiler to clang++ (common on macOS)
+# If you prefer g++, replace 'clang++' with 'g++'
+cmake -DCMAKE_CXX_STANDARD=20 -DCMAKE_CXX_COMPILER=$compiler -DBUILD_GUI=ON -LH -S ../
+
+# Now, try to build again
+make
