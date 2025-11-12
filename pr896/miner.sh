34b68c89093914ab2269d1c7a26b0f32996bdf08
randymcmillan <randymcmillan@protonmail.com>
2025-11-11 20:52:32 -0500

    ...

---
+++ pr896/miner.sh
@@ -0,0 +1,2 @@
+ ../contrib/signet/miner --cli './bin/bitcoin-cli -datadir=./signet_data' generate --address tb1quj2f4cdeczgsuqcuse0aehc8alp2zyqknfs8zg --grind-cmd './bin/bitcoin-util grind' --min-nbits --ongoing
+exit;
